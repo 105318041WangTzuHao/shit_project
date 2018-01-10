cd134e000318c08219e560d2b6df5afbfe4cb342
s106369008 <32257642+s106369008@users.noreply.github.com>
2018-01-10 18:54:12 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install python3-matplotlib
+sudo apt-get install python3-pip
+pip3 install --upgrade pip
+sudo pip3 install pandas
+pip3 install scipy
+pip3 install tensorflow
+pip3 install keras
+sudo pip3 install keras-tqdm
+pip3 install tqdm
+pip3 install --upgrade tensorflow
+sudo pip3 install ipywidgets
+sudo pip3 install h5py
+sudo apt-get install ipython3
